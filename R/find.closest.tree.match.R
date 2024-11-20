abde0cdd93621617f9e7a6adbb3ae7098e8a45f0
Thomas Guillerme <guillert@tcd.ie>
2024-11-20 15:36:19 +0000

    intialise find.closest.tree.match

---
+++ R/find.closest.tree.match.R
@@ -0,0 +1,21 @@
+#' @title find.closest.tree.match
+#'
+#' @description Find the closest partial match in a tree
+#'
+#' @param match a \code{"character"} string to match
+#' @param tree a \code{"phylo"} object to search in.
+#' @param split.fun the function for splitting names.
+#' @param distance optional, an algorithm for measuring the distance.
+#' 
+#' @details
+#' 
+#' @examples
+#'
+#' @seealso
+#' 
+#' @author Thomas Guillerme
+#' @export
+
+find.closest.tree.match <- function(match, tree, split.fun, distance) {
+    return(NULL)
+}
---
+++ tests/testthat/test-find.closest.tree.match.R
@@ -0,0 +1,3 @@
+test_that("find.closest.tree.match works", {
+    expect_null(find.closest.tree.match())
+})
