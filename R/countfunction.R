f63ce4ef4d60b259a00ebce05c862768667247c6
calebowski <cnscutt1@sheffield.ac.uk>
2024-11-21 18:50:56 +0000

    cleaned function

--- R/countfunction.R
+++ R/countfunction.R
@@ -1,30 +1,8 @@
-species_beaks <- algo_design_data$Species_beaks
-
-## load jetz tree tip labels
-jetz_tree <- algo_design_data$Jetz_tree
-jetz_tips <- jetz_tree$tip.label
-
-## create jetz dataframe splitting species and genus
-jetz_df <- data.frame(do.call(rbind, strsplit(jetz_tips, "_")))
-colnames(jetz_df) <- c("Genus", "Species")
-
-
-
-## create df for species beaks
-beaks_df <-  data.frame(do.call(rbind, strsplit(species_beaks, "_")))
-colnames(beaks_df) <- c("Genus", "Species")
-
-
-## count the number of species for each genus
-count_jetz <- aggregate(species_jetz ~ genus_jetz, data = jetz_df, FUN = function(x) length(unique(x)))
-
-
-
-## functionalise 
-count_species_per_genus <- function(data) {
+ ## input dataframe should be genera and species as separate columns
+ count_species_per_genus <- function(data) {
   # Check if the required columns exist
   
-  # Aggregate to count unique species per genus
+  # use aggregate function to count unique species per genus
   count <- aggregate(Species ~ Genus, data = data, FUN = function(x) length(unique(x)))
   
   # Rename the count column for clarity
@@ -32,24 +10,6 @@ count_species_per_genus <- function(data) {
   
   return(count)
 }
-
-## test usage 
-count_jetz <- count_species_per_genus(jetz_df)
-count_beaks <- count_species_per_genus(beaks_df)
-
-
-monotypic_genera_jetz <- count_jetz[count_jetz$Species_Count == 1, "Genus"]
-monotypic_genera_beaks <- count_beaks[count_beaks$Species_Count == 1, "Genus"]
-
-mismatches_jetz <- setdiff(monotypic_genera_jetz, monotypic_genera_beaks)
-mismatches_beaks <- setdiff(monotypic_genera_beaks, monotypic_genera_jetz)
-mismatches_list <- list(
-  Jetz_Mismatches = mismatches_jetz,
-  Beaks_Mismatches = mismatches_beaks
-)
-
-
-
 
 
 
