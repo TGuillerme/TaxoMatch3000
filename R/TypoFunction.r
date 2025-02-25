f9d720d4f9b26b0eb908ca0a65e261c3a0ba0933
Katherine Assersohn <k.assersohn@gmail.com>
2025-02-25 16:15:11 +0000

    Roxygen tags to typo function

--- R/TypoFunction.r
+++ R/TypoFunction.r
@@ -1,22 +1,43 @@
-# Load data
-load("data/algo_design_data.rda")
+#' Match species pairs based on Levenshtein distance
+#' 
+#' @author Katherine Assersohn
+#' 
+#' @description This function finds the most similar species pairs between two data frames based on the Levenshtein distance. 
+#' 
+#' @param focal_df A data frame containing the focal species. Should have species and genus as separate columns.
+#' @param tree_df A data frame containing the tree species to match with. Should have species and genus as separate columns.
+#' @param genus_column The column in `focal_df` and `tree_df` that contains the genus names.
+#' @param species_column The column in `focal_df` and `tree_df` that contains the species names.
+#' @param match_type The type of match to perform. Can be one of 'genus', 'species', or 'combined' (default). If 'combined', the function uses the sum of the genus and species distances to create a combined score.
+#' @param remove_duplicates Indicates whether duplicate matches should be removed. Default is `FALSE`. If TRUE, only the closest match is retained.
+#' 
+#' @return A data frame with columns for the focal species and its closest tree match based on Levenshtein distance, 
+#'         along with the corresponding genus, species, and combined distances. Duplicates are flagged and can be removed 
+#'         based on the `remove_duplicates` argument.
+#' 
+#' @example
+#' most_similar_pairs_df <- find_most_similar_pairs(focal, match,
+#'   genus_column = "Genus", species_column = "Species", remove_duplicates = TRUE, match_type = "combined")
+#' 
+#' # Extract exact matches where the combined distance is zero
+#' exact_matches <- most_similar_pairs_df %>% filter(Combined_Distance == 0)
+#' 
+#' # Extract matches with a combined distance less than or equal to 2
+# close_matches <- most_similar_pairs_df %>% filter(Combined_Distance > 0 & Combined_Distance <= 2)
+
+# Extract matches with a combined distance greater than or equal to 3
+# no_matches <- most_similar_pairs_df %>% filter(Combined_Distance >= 3)
+#' 
+#' @import dplyr
+#' @import tidyr
+#' @import stringdist
+#' 
+#' @export
 
 # Load libraries
 library(dplyr)
 library(tidyr)
 library(stringdist)
-
-#### Function to find the most similar pairs based on Levenshtein distance ####
-
-# The function takes 4 arguments: 2 dataframes to compare against, the column name to compare from, 
-# and whether you want to include or remove duplicates
-
-# The function finds the most similar species pairs between two data frames based on the Levenshtein distance. 
-# you can specify if you'd like to match based on the 'genus' or the 'species', or whether you'd like to match based on a minimised combined score (so genus similarity + species similarity)
-# since the function finds the closest match for each species in focal_df independently, it is possible for multiple
-# species in focal_df to be matched to the same species in tree_df. The function will flag where this 
-# is the case and you can optionally choose to remove duplicates, keeping only the matches with the smallest lv
-# It is probably worth double checking duplicates by calling duplicates <- attr(most_similar_pairs_df, "duplicates")
 
 find_most_similar_pairs <- function(focal_df, tree_df, genus_column, species_column,
                                     match_type = "combined", remove_duplicates = FALSE){
@@ -48,8 +69,8 @@ find_most_similar_pairs <- function(focal_df, tree_df, genus_column, species_col
     data.frame(
       Genus_Focal = focal_genus,
       Species_Focal = focal_species,
-      Genus_Tree = similar_match[[genus_column]],
-      Species_Tree = similar_match[[species_column]],
+      Genus_Match = similar_match[[genus_column]],
+      Species_Match = similar_match[[species_column]],
       Genus_Distance = genus_distances[min_index],
       Species_Distance = species_distances[min_index],
       Combined_Distance = matching_distances[min_index],
@@ -62,7 +83,7 @@ find_most_similar_pairs <- function(focal_df, tree_df, genus_column, species_col
   
   # Add a column to flag duplicates in the tree matches
   similar_pairs <- similar_pairs %>%
-    group_by(Genus_Tree, Species_Tree) %>%
+    group_by(Genus_Match, Species_Match) %>%
     mutate(Duplicate_Tree_Match = n() > 1) %>%
     ungroup()
   
@@ -73,7 +94,7 @@ find_most_similar_pairs <- function(focal_df, tree_df, genus_column, species_col
   # Remove duplicates if the option is set to TRUE.
   if (remove_duplicates) {
     similar_pairs <- similar_pairs %>%
-      group_by(Genus_Tree, Species_Tree) %>%
+      group_by(Genus_Match, Species_Match) %>%
       filter(if (match_type == "genus") {
                 Genus_Distance == min(Genus_Distance)
               } else if (match_type == "species") {
@@ -82,7 +103,7 @@ find_most_similar_pairs <- function(focal_df, tree_df, genus_column, species_col
                 Combined_Distance == min(Combined_Distance)
               }) %>%
       ungroup() %>%
-      distinct(Genus_Tree, Species_Tree, .keep_all = TRUE)
+      distinct(Genus_Match, Species_Match, .keep_all = TRUE)
   }
  
   # Add the duplicates as an attribute to the similar_pairs data frame
@@ -90,22 +111,3 @@ find_most_similar_pairs <- function(focal_df, tree_df, genus_column, species_col
   
   return(similar_pairs)
 }
-
-# Test
-data_names <- split_binomials(algo_design_data$Species_beaks)
-tree_names <- split_binomials(algo_design_data$Clements_tree$tip.label)
-
-most_similar_pairs_df <- find_most_similar_pairs(data_names, tree_names,
-                                                      "Genus", "Species", remove_duplicates = TRUE,
-                                                      match_type = "Combined")
-View(most_similar_pairs_df)
-
-# Extract the duplicates from the result
-# duplicates <- attr(most_similar_pairs_df, "duplicates")
-
-# Extract the exact matches from the result
-# exact_matches <- most_similar_pairs_df %>% filter(Combined_Distance == 0)
-
-# Extract matches with a combined distance of 1
-# close_matches <- most_similar_pairs_df %>% filter(Combined_Distance == 1)
-
